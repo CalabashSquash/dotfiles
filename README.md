b9b71394f99f9c1da1e9f564f2781a881c57cb2e
BenjaminPatch <benpatch@hotmail.com>
2021-09-29 14:11:04 +1000

    Add readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# My setup
+
+From a brand new Ubuntu installation, run:
+```
+cd
+git clone https://github.com/mynameuhh/dotfiles.git
+cd dotfiles
+./newComputerSetup.sh
+```
+Then `reboot` and open with i3.
+
+## Installs
+- i3 + my i3 config
+- Git + my git aliases
+- Python3 + pip3
+- Redshift
+- Discord
+- Spotify
+- VS code
+- Firefox
+- neovim + my config + git default editor + alias to `vim`
+
--- newComputerSetup.sh
+++ newComputerSetup.sh
@@ -6,7 +6,6 @@ cd
 sudo apt-get update
 sudo apt-get -y install i3
 sudo apt-get -y install git
-git config --global core.editor "nvim"
 mkdir ~/.config # Probably already exists. That is fine
 cd ~/.config
 mkdir i3
@@ -26,6 +25,7 @@ cd ~/.config
 mkdir nvim
 cd ~/dotfiles
 cp neovim/neovimConfig ~/.config/nvim/init.vim
+git config --global core.editor "nvim"
 
 cd
 echo "alias vim=\"nvim\"" >> .bashrc
