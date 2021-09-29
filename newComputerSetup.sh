ac5e45855a81669d23af6f93078ba934e826fffa
BenjaminPatch <benpatch@hotmail.com>
2021-09-29 13:45:08 +1000

    Add aliases

--- newComputerSetup.sh
+++ newComputerSetup.sh
@@ -4,7 +4,6 @@ cd
 sudo apt-get update
 sudo apt-get install i3
 sudo apt-get install git
-git clone https://github.com/mynameuhh/dotfiles.git
 mkdir ~/.config # Probably already exists. That is fine
 cd ~/.config
 mkdir i3
@@ -13,4 +12,15 @@ cp i3/config ~/.config/i3/
 sudo apt-get -y install python3-pip
 sudo apt-get -y install python3
 sudo apt-get install redshift
+sudo snap install discord
+sudo snap install spotify
+sudo snap install --classic code
+sudo apt install firefox
+cd
+echo "alias g-s=\"git status\"" >> .bashrc
+echo "alias g-pl=\"git pull\"" >> .bashrc
+echo "alias g-ps=\"git push\"" >> .bashrc
+echo "alias g-c=\"git commit\"" >> .bashrc
+echo "alias g-full=\"git add -u && git commit && git push\"" >> .bashrc
+source .bashrc
 
